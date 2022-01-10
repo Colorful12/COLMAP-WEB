b3402ebe394cdb27386b9dce8eec8f5e7a7a804b
Colorful12 <cat37280@gmail.com>
2022-01-11 05:22:44 +0900

    make it possible to upload files

--- project/__init__.py
+++ project/__init__.py
@@ -1,13 +1,33 @@
 # projectの実体
 
-import flask import Flask
+import os
+from flask import Flask, render_template,request
+from werkzeug.utils import secure_filename #
 
-def create_app():
-    
+def create_app(test_config=None):
+
+    app = Flask(__name__, instance_relative_config=True)
+    UPLOAD_FOLDER = 'upload_folder/'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
     @app.route("/")
-    def hello():
-        return "Hello, World!!"
+    def root():
+        return "This is root page."
 
-    from . import app
-    app.name_of_the_program()
+    @app.route('/upload')
+    def upload():
+        return render_template('upload.html')
+
+    @app.route('/upload_file',methods=['POST'])
+    def upload_file():
+        os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+        if request.method == 'POST':
+            files = request.files.getlist('file')
+            for file in files:
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
+            return 'file uploaded successfully'
+    
+    #from . import model
+    #model.name_of_the_program()
+
+    return app
