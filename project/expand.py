8a87f0d7e44261249e9732fb117bb72b33897dec
Colorful12 <cat37280@gmail.com>
2022-01-12 03:24:33 +0900

    change the name of model.py to expand.py

---
+++ project/expand.py
@@ -0,0 +1,8 @@
+# colmapで生成したデータを読み込む & その他2D, 3Dデータの配置を行うページ
+
+from flask import(
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+bp = Blueprint("expand", __name__)
